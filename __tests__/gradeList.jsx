84f135f96854485560deb1358b873bc1f66c3c2e
felix.ravella <felix.ravella@gmail.com>
2023-06-14 16:04:07 +0200

    chore: cleaner test

--- __tests__/gradeList.jsx
+++ __tests__/gradeList.jsx
@@ -1,3 +1,4 @@
+import { describe, expect, it } from "vitest";
 
 /* ---- DOESN'T WORK    ReferenceError: expect is not defined
 
@@ -6,10 +7,18 @@ import '@testing-library/jest-dom'
 import * as React from 'react'
 import {render, fireEvent, screen} from '@testing-library/react'
 import { GradeList } from '../src/grades/GradeList'
-import { expect } from 'vitest'
 
 test('Displays grade list', () => {
   render(<GradeList></GradeList>)
   expect(screen.getByText("Not here")).toBeNull()
 })
 */
+
+
+
+describe('useGradeStore', () => {
+  it('should pass'), () => {
+    
+    expect(true).toBeTruthy();
+  }
+})
