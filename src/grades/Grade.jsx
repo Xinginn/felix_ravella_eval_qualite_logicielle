489b72bd35798b726b2780126e458bfad9a6b5ab
felix.ravella <felix.ravella@gmail.com>
2023-06-14 11:57:35 +0200

    feat: import style

--- src/grades/Grade.jsx
+++ src/grades/Grade.jsx
@@ -1,11 +1,25 @@
+import "../assets/styles.css"
+
 export function Grade({gradeData}) {
+
+  const styles = {
+    red:{
+      backgroundColor: "red",
+    }
+  }
+
+
+  function getGradeClass() {
+    return "red"
+  }
 
   return (
     <>
-      <div>
+      <div className={getGradeClass()}>
         <b>{gradeData.title}:</b><span >{gradeData.value}</span>
         <i>"{gradeData.comment}"</i>
       </div>
     </>
   )
 }
+
