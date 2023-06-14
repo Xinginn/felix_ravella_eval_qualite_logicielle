12bca100d9f4ddee64a0dfdc9ca86efbb5ba91f4
felix.ravella <felix.ravella@gmail.com>
2023-06-14 12:00:47 +0200

    feat(grade): change color depending on grade value

--- src/grades/Grade.jsx
+++ src/grades/Grade.jsx
@@ -2,15 +2,16 @@ import "../assets/styles.css"
 
 export function Grade({gradeData}) {
 
-  const styles = {
-    red:{
-      backgroundColor: "red",
-    }
-  }
-
-
   function getGradeClass() {
-    return "red"
+    if(gradeData.value < 8){
+      return "red";
+    } else if (gradeData.value < 10) {
+      return "orange";
+    } else if (gradeData.value < 13) {
+      return "yellow";
+    } else {
+      return "green";
+    }
   }
 
   return (
